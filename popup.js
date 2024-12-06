6a12a53bd272b9ff88f7596705c3b5d95152e0ec
Elizabeth.Lakata@gmail.com <Elizabeth.Lakata@gmail.com>
2024-12-05 22:44:22 -0500

    first commit

---
+++ popup.js
@@ -0,0 +1,46 @@
+console.clear
+console.log('This is a popup from EL - 20241128!')
+
+console.log("comment outside searchWord function :)") /* does print */
+
+/** search the current website for words */
+const searchWord = () => {
+  
+    console.log("comment inside searchWord function :)") /* doesn't print */
+
+    const text = document.body.textContent ; 
+    const flower = text.includes("flower" || "Flower") ;
+
+    /* get all the text data on the page */
+    const allInnerText = document.body.innerText ;
+
+    return{
+      oneWord: flower,
+      otherData: allInnerText,
+      otherData2: 4
+    }
+}
+
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id},
+        func: searchWord
+    }, (result) => {
+                     
+        if (result[0].result.oneWord){
+          
+            console.log("found some flowers!")
+            const pageResult = "This webpage has flower info";
+            document.getElementById("foundWord").innerText = pageResult; 
+
+        } else {
+            console.log("didn't find flowers")
+        }
+    }
+)
+
+}
+
+
+)
+
