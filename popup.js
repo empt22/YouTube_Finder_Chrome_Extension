a2401ef328d976f2413eeeae7062be43e54731f3
Elizabeth.Lakata@gmail.com <Elizabeth.Lakata@gmail.com>
2024-12-05 23:07:16 -0500

    changed comment

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 console.clear
-console.log('This is a popup from EL - 20241128!')
+console.log('This is a popup from EL - 20241205!')
 
 console.log("comment outside searchWord function :)") /* does print */
 
